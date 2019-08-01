906d97696924553a53ec9396dc811ed87f5881f7
Kyle Knaggs <kyle@adfoam.com>
2019-08-01 16:35:00 -0400

    Move saving of todos into own function.
    Todos are saved in window.localStorage.

--- js/app.js
+++ js/app.js
@@ -70,7 +70,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -92,6 +91,7 @@ jQuery(function ($) {
 			});
 
 			this.render();
+			this.saveTodos();
 		},
 		getActiveTodos: function () {
 			return this.todos.filter(function (todo) {
@@ -114,10 +114,14 @@ jQuery(function ($) {
 
 			return this.todos;
 		},
+		saveTodos: function () {
+			util.store('todos-jquery', this.todos);
+		},
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
 			this.render();
+			this.saveTodos();
 		},
 		// accepts an element from inside the `.item` div and
 		// returns the corresponding index in the `todos` array
@@ -149,11 +153,13 @@ jQuery(function ($) {
 			$input.val('');
 
 			this.render();
+			this.saveTodos();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
 			this.render();
+			this.saveTodos();
 		},
 		edit: function (e) {
 			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
@@ -185,10 +191,12 @@ jQuery(function ($) {
 			}
 
 			this.render();
+			this.saveTodos();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
 			this.render();
+			this.saveTodos();
 		}
 	};
 
